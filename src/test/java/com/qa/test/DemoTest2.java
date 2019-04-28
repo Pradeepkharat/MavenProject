2f17eb3dcfe6f26a33dbf4ac3459d08a85a74f17
Pradeep Kharat <pradeepkharat91@gmail.com>
2019-04-28 11:59:14 +0530

    maven project code

---
+++ src/test/java/com/qa/test/DemoTest2.java
@@ -0,0 +1,42 @@
+package com.qa.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+	
+	
+	@Test
+	public void sum()
+	{
+		int a=10;
+		int b=20;
+		Assert.assertEquals(30, a+b);
+		System.out.println("Sum");
+	}
+	@Test
+	public void sub()
+	{
+		int a=10;
+		int b=20;
+		Assert.assertEquals(10, b-a);
+		System.out.println("Sub");
+	}
+	@Test
+	public void mul()
+	{
+		int a=10;
+		int b=20;
+		Assert.assertEquals(200, a*b);
+		System.out.println("Mul");
+	}
+	@Test
+	public void div()
+	{
+		int a=10;
+		int b=20;
+		Assert.assertEquals(2, b/a);
+		System.out.println("Div");
+	}
+
+}
